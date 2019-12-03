9d64448c0f9a1617548da490872b4a83171e9b05
markbello <mcbello51286@gmail.com>
2019-12-02 22:00:25 -0500

    feat(groupInfo): make generic

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,10 @@
 import baseTheme from '@intergroupsite/gatsby-theme-intergroup-site/src/gatsby-plugin-theme-ui';
 import merge from 'lodash.merge';
 
+// This file allows you to override default styling from @intergroupsite/gatsby-theme-intergroup-site
+
 export default merge({}, baseTheme, {
-	colors: {
-		primary: '#e64242',
-	},
+	// colors: {
+	// 	primary: '#e64242',
+	// },
 })
